9d56687dd8c84a52409300d46a2cdc19d9c9551e
codynego <emonenaabednego@gmail.com>
2023-05-04 19:42:29 +0500

    minimum operation problem solved

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+"""
+Minimum Operations
+"""
+
+
+def minOperations(n: int) -> int:
+    """
+    write a method that calculates the fewest number of
+    operations needed to result in exactly n H characters in the file
+
+    Return:
+        returns an int
+    """
+    operation_count = 0
+    characters = "H"
+    clipboard = ""
+
+    for i in range(n):
+        if len(characters) > n:
+            return (0)
+
+        if len(characters) == n:
+            break
+        else:
+            if n % len(characters) == 0:
+                clipboard = characters
+                characters += clipboard
+                operation_count += 2
+
+            else:
+                characters += clipboard
+                operation_count += 1
+
+    return(operation_count)
