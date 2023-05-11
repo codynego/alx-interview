39b151058299934640ef8868c604f28b1aaff24b
codynego <emonenaabednego@gmail.com>
2023-05-11 07:36:57 +0100

    log parser

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+"""
+log parsing
+"""
+import re
+import sys
+
+
+def validate_input(inputs):
+    pattern = (
+        r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[.+\] "GET /projects/260 '
+        r'HTTP/1\.1" \d{3} \d+$'
+    )
+    match = re.match(pattern, inputs)
+    if match:
+        return True
+    else:
+        return False
+
+
+def log_parser():
+    line_count = 0
+    total_size = 0
+    codes = {}
+    status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
+    for line in sys.stdin:
+        if validate_input(line):
+            line_count += 1
+            input_list = line.split("/")
+            filesize = input_list[-1].split(' ')[-1]
+            status_code = input_list[-1].split(' ')[-2]
+            total_size += int(filesize)
+            if status_code in codes:
+                codes[status_code] += 1
+            else:
+                codes[status_code] = 0
+                codes[status_code] += 1
+            if line_count == 10:
+                print(f"File size: {total_size}")
+                codes = {key: codes[key] for key in sorted(codes)}
+                for k, v in codes.items():
+                    print(f"{k}: {v}")
+                line_count = 0
+        else:
+            continue
+    return 0
+
+
+if __name__ == "__main__":
+    log_parser()
