c5fa73315b28c057ef123a80663deaa610cbc19f
codynego <emonenaabednego@gmail.com>
2023-05-17 19:49:30 +0100

    made another changes

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -10,6 +10,6 @@ def validUTF8(data):
     check if a data is a valid utf-8 encoding
     """
     for i in data:
-        if len(bin(i)[2:]) > 8 or type(i) != int:
+        if len(bin(i)[2:]) > 8 or type(i) != int or i > 0x10ffff:
             return False
     return True
